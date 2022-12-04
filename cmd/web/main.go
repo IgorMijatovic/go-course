19cb634d2d8ba15bba7fbb960f7a6b88708412c4
Igor Mijatovic <Igor.Mijatovic@jsmd-group.com>
2022-12-04 23:48:34 +0100

    Add layouts

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,8 +11,8 @@ import (
 const portNumber = ":8080";
 
 func main() {
-	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
+	http.HandleFunc("/", handlers.Home)
 
 	_ = http.ListenAndServe(portNumber, nil)
 }
